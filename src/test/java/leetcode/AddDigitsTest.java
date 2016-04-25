f922b6285c8dd5cfe148e65b978427dc7e592ee0
p.chen <p.chen@qunar.com>
2016-04-25 19:31:18 +0800

    add testng

---
+++ src/test/java/leetcode/AddDigitsTest.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * Created by p.chen on 2016/4/25.
+ */
+public class AddDigitsTest {
+
+    @org.testng.annotations.BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @org.testng.annotations.Test
+    public void testAddDigits() throws Exception {
+
+    }
+}
