a941cbddad413407847804278257b2eb4ed9337a
p.chen <p.chen@qunar.com>
2016-05-09 16:06:36 +0800

    same tree

--- src/main/java/leetcode/SameTree.java
+++ src/main/java/leetcode/SameTree.java
@@ -2,9 +2,6 @@ package leetcode;
 
 import leetcode.datastructure.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by p.chen on 2016/5/4.
  * Given two binary trees, write a function to check if they are equal or not.
@@ -14,20 +11,27 @@ import java.util.List;
 public class SameTree {
     /**
      * traversal two trees use two check the result
+     *
      * @param p
      * @param q
      * @return
      */
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return true;
+
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+
+        return isSameTree(p.left, q.left)?isSameTree(p.right, q.right):false;
     }
 
-    /**
-     *
-     * @param r
-     */
-    private void LDR(TreeNode r){
-        List<Integer> result = new ArrayList<>();
-
-    }
 }
